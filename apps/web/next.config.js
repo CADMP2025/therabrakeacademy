e905346fc669101a875f20e0b7048afda056d923
MHDF Venture Holdings <222900326+CADMP2025@users.noreply.github.com>
2025-09-17 13:06:04 +0000

    package.json fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['ynizozzfjkocvbycxwvo.supabase.co'],
   },
+  swcMinify: false,
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  }
 }
 
 module.exports = nextConfig
